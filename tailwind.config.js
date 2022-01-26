fe4eca91bf6d5204c8160141daeb00662fc71bba
Lewin <lewin.sieben@raabeschule.net>
2022-01-26 18:49:06 +0100

    Pflegeteam Braunschweig Intial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 module.exports = {
-  purge: ['themes/layouts/**/*.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: ['themes/layouts/**/*.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+        colors: {
+            'white': '#ffffff',
+            "pg-gray": "#ebeee1",
+            'pg-green': {
+                100: '#f8fbee',
+                500: '#a5c928',
+                900: '#5d711a',
+            },
+        },
+        fontFamily: {
+            sans: ['Inter', 'sans-serif'],
+            serif: ['Recoleta Alt', 'serif'],
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
