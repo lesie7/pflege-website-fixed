a501fe28da732a64abd0680e80c0f35ec4c9753a
Lewin <lewin.sieben@raabeschule.net>
2022-09-27 21:32:07 +0200

    NavTopBar with Infos; Telephone Button bottom right

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,44 @@
 module.exports = {
-    important: true,
-    purge: [
-        'themes/layouts/**/*.html',
-        'themes/layouts/*.html',
-    ],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            backgroundImage: {
-                'hero': "url('/img/header-img.jpg')",
-               },
-            dropShadow: {
-                '3xl': '0 35px 35px rgba(93, 113, 26, 0.25)',
-              }
+  important: true,
+  purge: ["themes/pflege/layouts/**/*.html", "themes/pflege/layouts/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        hero: "url('/img/header-img.jpg')",
+      },
+      dropShadow: {
+        "3xl": "0 35px 35px rgba(93, 113, 26, 0.25)",
+      },
+      colors: {
+        white: "#ffffff",
+        "pg-gray": "#ebeee1",
+        "text-color": "#4D525D",
+        "gradient-green": "#112715",
+        "pg-green": {
+          100: "#f8fbee",
+          300: "#EBEEE1",
+          500: "#a5c928",
+          700: "#85A029",
+          900: "#5d711a",
         },
-        colors: {
-            'white': '#ffffff',
-            "pg-gray": "#ebeee1",
-            "text-color": "#4D525D",
-            "gradient-green": "#112715",
-            'pg-green': {
-                100: '#f8fbee',
-                300: '#EBEEE1',
-                500: '#a5c928',
-                700: "#85A029",
-                900: '#5d711a',
-            },
-            "grey" : {
-                50: "#F9FAFB",
-                100: "#F3F4F6",
-                200: "#E5E7EB",
-                300: "#D1D5DB",
-                900: "#111827",
-            },
-            "green-gray" : "EBEEE2",
+        grey: {
+          50: "#F9FAFB",
+          100: "#F3F4F6",
+          200: "#E5E7EB",
+          300: "#D1D5DB",
+          900: "#111827",
         },
-        fontFamily: {
-            sans: ['Inter', 'sans-serif'],
-            serif: ['Merriweather', 'serif'],
-        },
+        "green-gray": "EBEEE2",
+      },
     },
-    variants: {
-        extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      serif: ["Merriweather", "serif"],
     },
-    plugins: [],
-}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
